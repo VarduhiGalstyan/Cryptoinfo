05eaec447216dab52cfa292d79bdbc931f89a308
Varduhi <varduhigalstyann71@gmail.com>
2024-12-23 11:53:39 +0400

    6516546516515

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<!-- layouts/default.vue -->
+<template>
+    <div>
+      <head>
+        <meta name="description" :content="apiStore.myHeader?.meta_description_en || ''" />
+        <meta name="keywords" :content="apiStore.myHeader?.meta_keywords_en || ''" />
+        <link rel="icon" :href="apiStore.myHeader?.favicon || ''" />
+      </head>
+      <NuxtPage />
+    </div>
+  </template>
+  
+  <script setup>
+  import { useApi } from "~/stores/api";
+  import { onMounted } from "vue";
+  
+  const apiStore = useApi();
+  onMounted(async () => {
+    await apiStore.fetchData();
+  });  
+  </script>
+  
